0a0c97ef74fc8dfeb69f3cfe1062f0dffd3b6724
qbaebler <qbaebler@uw.edu>
2024-04-30 18:06:07 -0700

    fixed appjs

--- App.js
+++ App.js
@@ -25,7 +25,7 @@ function InsideNav() {
   const navigation = useNavigation();
   return (
     <InsideStack.Navigator>
-      <InsideStack.Screen name="Home" component={DasherScreen} options={{ headerShown: false }} />
+      <InsideStack.Screen name="Home" component={HomeScreen} options={{ headerShown: false }} />
       <InsideStack.Screen name="FoodLocker" component={FoodLockerScreen} options={{ headerShown: false }} />
       <InsideStack.Screen name="RestaurantDetail" component={RestaurantDetailScreen} options={{ headerShown: false }} />
       <InsideStack.Screen
@@ -67,13 +67,13 @@ function InsideLayout() {
 }
 
 const App = () => {
-  const [user, setUser] = useState("hi");
+  const [user, setUser] = useState("");
 
-  /*useEffect(() => {
+  useEffect(() => {
     onAuthStateChanged(FIREBASE_AUTH, (user) => {
       setUser(user);
     })
-  }, []); */
+  }, []); 
 
   return (
     <NavigationContainer>
